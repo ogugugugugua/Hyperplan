cfd42796ec55eebac303789f7a0e462ff9aeaffb
XieYuLin <xieyl_github@163.com>
2019-12-01 17:20:41 +0100

    text classification based on simple softmax

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Hyperplan
 Polytech Nantes - INFO4-SILR - Projet Transversal
 
+stopWordsFR: stop words for the french language 
+
+texte.txt: the text file whose category is to be determined
+
+categories.txt: all existing possible categories 
+
+test.py: program for finding the corresponding category of the text file
 
 Author: Yulin XIE
--- test.py
+++ test.py
@@ -1,57 +1,20 @@
-# encoding=utf8
+# -*- coding: UTF-8 -*-
 
 import math
+from filecmp import cmp
 import pandas as pd
 import numpy as np
 import random
 import time
 import os
-
 import collections
-
-
-def stopwords(filePath):
-    pass
-
-
-def rm_stopwords(self, file_path, word_dict):
-    # read stop word dict and save in stop_dict
-    stop_dict = {}
-    with open(word_dict) as d:
-        for word in d:
-            stop_dict[word.strip("\n")] = 1
-
-    # # remove tmp file if exists
-    # if os.path.exists(file_path + ".tmp"):
-    #     os.remove(file_path + ".tmp")
-    #
-    # print
-    # "now remove stop words in %s." % file_path
-    # # read source file and rm stop word for each line.
-    # with nested(open(file_path), open(file_path + ".tmp", "a+"))  as (f1, f2):
-    #     for line in f1:
-    #         tmp_list = []  # save words not in stop dict
-    #         words = line.split()
-    #         for word in words[1:]:
-    #             if word not in stop_dict:
-    #                 tmp_list.append(word)
-    #         words_without_stop = " ".join(tmp_list)
-    #         f2.write(words[0] + " " + words_without_stop + "\n")
-    #
-    # # overwrite origin file with file been removed stop words
-    # shutil.move(file_path + ".tmp", file_path)
-    # print
-    # "stop words in %s has been removed." % file_path
-    # return
 
 
 def softmax(x):
     """
     Compute the softmax function for each row of the input x.
-
     Arguments:
     x -- A N dimensional vector or M x N dimensional numpy matrix.
-
     Return:
     x -- You are allowed to modify x in-place
     """
@@ -80,47 +43,64 @@ def softmax(x):
     return x
 
 
-def findCategory(filePath):
+def findCategory(filePath, stopWordsPath, categoriesPath):
+    # get the text file content
     fileContent = ""
     with open(filePath) as file:
         for line in file:
             fileContent += line
-    # print("fileContent:\n", fileContent)
+
+    # count times of appearance of each word
     frequency = dict(collections.Counter(fileContent.split()))
+
+    # get the list of each single word in the file
     keys = list(frequency)
+
+    # get the times of appearance of each word
     values = list(frequency.values())
+
+    # count the possibility of each word by using softmax
     softmaxValues = softmax(np.array(values))
+
+    # translate to softmax probability
     for i, k in enumerate(frequency):
         frequency[k] = softmaxValues[i]
-    return sorted(frequency.items(), key=lambda item: item[1], reverse=True)
+
+    # get meaningless vocabulary
+    stopword = []
+    with open(stopWordsPath) as file:
+        for word in file:
+            stopword.append(word[:-2])
+
+    # 1st version for deleting meaningless words, which only works for part of the text for some unclear reasons
+    # frequencyWithoutStopWords = frequency.copy()
+    # for i, k in enumerate(frequency):
+    #     if k in stopword:
+    #         del frequencyWithoutStopWords[k]
+
+    # 2nd version for deleting meaningless words, which works fine
+    frequencyWithoutStopWords = []
+    for i, k in enumerate(frequency):
+        if k not in stopword:
+            frequencyWithoutStopWords.append((k, frequency[k]))
+    frequencyWithoutStopWords = dict(frequencyWithoutStopWords)
+
+    # sort the words by its corresponding possibility
+    frequencyWithoutStopWords = sorted(frequencyWithoutStopWords.items(), key=lambda x: x[1], reverse=True)
+
+    # get the whole default categories
+    categories = []
+    with open(categoriesPath) as file:
+        for category in file:
+            categories.append(category[:-1])
+
+    # determine category
+    for i, (word, percentage) in enumerate(frequencyWithoutStopWords):
+        if word in categories:
+            # we got the category by finding the most possible word in the list of category
+            return word
+    return None
 
 
 if __name__ == "__main__":
-    # # x = np.array([[2, 8, 7, 6], [64, 78, 13, 4]])
-    # # print(x)
-    # # print(softmax(x))
-    #
-    # text = "I'm a hand some boy! I'm a hand some boy!I'm a ! \n I'm a hand some boy!"
-    # frequency = dict(collections.Counter(text.split()))
-    # keys = list(frequency)
-    # values = list(frequency.values())
-    # softmaxValues = softmax(np.array(values))
-    #
-    # print("values ", values, "\nsoftmaxValues ", softmaxValues)
-    #
-    # # print("keys ",keys)
-    # # print("frequency ",frequency)
-    # # print("frequency[keys[0]] ",frequency[keys[0]],"\n\n\n")
-    #
-    # for i, k in enumerate(frequency):
-    #     frequency[k] = softmaxValues[i]
-    #
-    # print(sorted(frequency.items(), key=lambda item: item[1], reverse=True))
-    # print(sorted(frequency.items(), key=lambda item: item[1]))
-    #
-    # # frequency = collections.defaultdict(int)
-    # # text = "I'm a hand some boy!"
-    # # for word in text.split():
-    # #     frequency[word] += 1
-    # # print(frequency)
-    print(findCategory("texte.txt"))
+    print("category: ", findCategory("texte.txt", "stopWordsFR.txt", "categories.txt"))
